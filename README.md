82be20d39980e35c7d62e1871f57a70ed23a4ec1
Barrett <baxance@protonmail.com>
2021-04-06 22:11:58 -0700

    lab12

---
+++ README.md
@@ -0,0 +1,3 @@
+# Songr App
+
+`gradle bootrun` to run app and then wait a year or two for it to maybe work
--- src/main/java/com/baxance/songr/Album.java
+++ src/main/java/com/baxance/songr/Album.java
@@ -1,13 +1,24 @@
 package com.baxance.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
     String title;
     String artist;
     int songCount;
     int length;
     String imageURL;
 
-public Album(String title, String artist, int songCount, int length, String imageURL){
+    public Album(String title, String artist, int songCount, int length, String imageURL){
     this.title = title;
     this.artist = artist;
     this.songCount = songCount;
@@ -15,31 +26,55 @@ public Album(String title, String artist, int songCount, int length, String imag
     this.imageURL = imageURL;
 }
 
+Album(){}
+
+public long getId() {
+    return id;
+}
+
+//public void setId(long id) {
+//    this.id = id;
+//}
+
 public String getTitle(){ // <- getter
     return title;
 }
 
-/*
-public String setTitle(String title){ // <- setter
-    this.title;
-}
- */
+//public void setTitle(String title) {
+//    this.title = title;
+//}
 
 public String getArtist(){
     return artist;
 }
 
+//public void setArtist(String artist) {
+//    this.artist = artist;
+//}
+
 public int getSongCount(){
     return songCount;
 }
+
+//public void setSongCount(int songCount) {
+//    this.songCount = songCount;
+//}
 
 public int getLength(){
     return length;
 }
 
+//public void setLength(int length) {
+//    this.length = length;
+//}
+
 public String getImageURL(){
     return imageURL;
 }
+
+//public void setImageURL(String imageURL) {
+//    this.imageURL = imageURL;
+//}
 
 }
 
